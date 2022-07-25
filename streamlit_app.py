9a7063c3a77d3bdbd82120a615bcadf134fe704f
Lini Lim <109926604+linialim@users.noreply.github.com>
2022-07-24 19:29:07 -0500

    Update streamlit_app.py
    add emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Ranged Egg')
 
