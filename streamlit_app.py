810a88d6636f9ed00d95bb5fb1214cd087d8975d
Lini Lim <109926604+linialim@users.noreply.github.com>
2022-07-24 19:30:38 -0500

    Update streamlit_app.py
    add menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,6 @@ streamlit.header('Breakfast Menu')
 
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Ranged Egg')
+streamlit.text('🐔Hard-Boiled Free-Ranged Egg')
+streamlit.text('🥑🍞Avocado Toast')
 
