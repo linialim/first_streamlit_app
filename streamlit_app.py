f66134ec8c030581a8d7c763ba5ac1e2389c7cfd
Lini Lim <109926604+linialim@users.noreply.github.com>
2022-08-07 09:25:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,7 @@ if streamlit.button('Add a fruit to the list'):
     
 # streamlit.write('Thanks for adding ', add_my_fruit)
 
-# don't run anything past here
+# don't run anything past here.
 streamlit.stop()
 
 
